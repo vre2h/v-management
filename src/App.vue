6f35a663cdd127c9f2e6ed8919e8d99df8f603e1
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-04-13 15:10:22 +0400

    Add menu

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <ul>
-      <li><b-link href="/">/</b-link></li>
-      <li><b-link href="/home">/home</b-link></li>
-    </ul>
-    <b-alert variant="success" show>Success Alert</b-alert>
+    <Menu />
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Menu from "./components/Menu.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Menu
+  }
 };
 </script>
 
@@ -23,6 +23,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin: 60px;
 }
 </style>
