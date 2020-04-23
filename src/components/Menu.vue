0064ef5221093197877a71f7db7bc4c64c91c6a5
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-04-23 04:07:53 +0400

    Fix issue with redirect

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -8,7 +8,7 @@
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="/timer">Timer</b-nav-item>
+            <b-nav-item to="/timer">Timer</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
