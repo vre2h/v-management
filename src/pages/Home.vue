92234d05c1f6e4359963489340a2622e3c8aebeb
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-05-01 22:43:50 +0400

    fix timer getting from i18next

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <b-container fluid="sm">
     <b-jumbotron :header="$t('vManage')" header-level="3">
-      <p>{{ $t("welcome.greeting") }}</p>
-      <p>{{ $t("welcome.offer") }}</p>
-      <router-link :to="{ name: 'timer' }">{{ $t("timer") }}</router-link>
+      <p>{{ $t('welcome.greeting') }}</p>
+      <p>{{ $t('welcome.offer') }}</p>
+      <router-link :to="{ name: 'timer' }">{{ $t('menu.timer') }}</router-link>
     </b-jumbotron>
   </b-container>
 </template>
